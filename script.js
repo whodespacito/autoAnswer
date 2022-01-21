74856615ed7952f6ad25d0a8c8e48edf8253b82e
whodespacito <58792361+whodespacito@users.noreply.github.com>
2022-01-21 08:42:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 window.start = function() {
-  window.always = setInterval(() => {
-    if (document.getElementById("next-btn")) {
-      document.getElementById("next-btn").click()
-    } else {
-      var number = Math.floor(Math.random() * 3);
-      var choices = document.querySelectorAll(".choice")
-      choices[number].click()
-    }
-  },30000)
+  if (window.isItOn === false) {
+    window.always = setInterval(() => {
+      if (document.getElementById("next-btn")) {
+        document.getElementById("next-btn").click()
+      } else {
+        var number = Math.floor(Math.random() * 3);
+        var choices = document.querySelectorAll(".choice")
+        choices[number].click()
+      }
+      window.isItOn = true
+    },30000)
+  } else {
+    clearInterval(always)
+    window.isItOn = false
+  }
 }
 window.stop = function() {
   clearInterval(always)
