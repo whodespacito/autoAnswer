c3efadfae0c83ea496c20f3e2418a54f867029f3
whodespacito <58792361+whodespacito@users.noreply.github.com>
2022-06-10 14:09:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+window.sleep = function (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 window.start = function() {
   window.always = setInterval(() => {
     if (document.getElementById("next-btn")) {
       document.querySelector(".answer").click()
+      await sleep(5000)
       document.getElementById("next-btn").click()
     } else {
       var number = Math.floor(Math.random() * 3);
