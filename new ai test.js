45102a4842c90d7c51a761a2d1f62294b11ea321
whodespacito <58792361+whodespacito@users.noreply.github.com>
2023-09-13 14:05:02 -0400

    testing keeping keys private

--- new ai test.js
+++ new ai test.js
@@ -33,7 +33,7 @@ async function query(data) {
     const response = await fetch(
         "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2",
         {
-            headers: { Authorization: "Bearer YOUR_TOKEN_HERE" },
+            headers: { Authorization: `Bearer ${key}` },
             method: "POST",
             body: JSON.stringify(data),
         }
@@ -41,6 +41,7 @@ async function query(data) {
     const result = await response.json();
     return result;
 }
+console.log(key)
 
 //i took this from stackoverflow
 //in stackoverflow we trust
